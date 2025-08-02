25595c69a56c873afe0d6fa5537e3a5b9e20f0cd
TatianAnat <TatianAnat@yandex.ru>
2025-08-03 02:04:40 +0400

    practice srteam2

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -26,9 +29,31 @@ public class Main {
         List<Integer> resultFromArray = Arrays.stream(arr)
                 .map(Integer::valueOf)
                 .filter(new CheckEven().and(new CheckMoreThan9()))
+                //будет выводить всё, что в него попало. Чётные больше 9
+                .peek(System.out::println)
                 .map(i -> i * i)
                 .toList();
         System.out.println("resultFromArray = " + resultFromArray);
+//проверяем чисо 111 на чётность
+        System.out.println("new CheckEven().test(111) = " + new CheckEven().test(111));
+        //переводит из строчки в число и считает количество необходимых элементов
+        Function<String, Integer> countSymbols = new Function<>(){
+            @Override
+            public Integer apply(String s) {
+                return s.length();
+            }
+        };
+        System.out.println("countSymbols.apply(\"123456\") = " + countSymbols.apply("123456"));
+
+        //соплаер. Стрим, который будет делать бесконечное число 9-к. в данном случае 9 девяток
+        System.out.println(" 9_9 = " + Stream.generate(() -> 9).limit(9).toList());
+
+        //соплаер. Стрим, который будет делать в данном случае 9 рандомных чисел
+        System.out.println(" 9_9 = " + Stream.generate(() -> Math.random()).limit(9).toList());
+
+        System.out.println(" 9_9 = " + Stream.generate(Math::random).limit(9).toList());
+
+        System.out.println("ints = " + IntStream.of(1,4,9,18).filter(i -> i % 3 == 0).boxed().toList());
     }
 
     static class CheckEven implements Predicate<Integer> {
