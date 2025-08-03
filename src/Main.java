8a30fb0ca3784ae0fe5e6a95072552c3bb61909c
TatianAnat <TatianAnat@yandex.ru>
2025-08-04 00:47:39 +0400

    practice srteam3

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
@@ -37,7 +38,7 @@ public class Main {
 //проверяем чисо 111 на чётность
         System.out.println("new CheckEven().test(111) = " + new CheckEven().test(111));
         //переводит из строчки в число и считает количество необходимых элементов
-        Function<String, Integer> countSymbols = new Function<>(){
+        Function<String, Integer> countSymbols = new Function<>() {
             @Override
             public Integer apply(String s) {
                 return s.length();
@@ -53,8 +54,42 @@ public class Main {
 
         System.out.println(" 9_9 = " + Stream.generate(Math::random).limit(9).toList());
 
-        System.out.println("ints = " + IntStream.of(1,4,9,18).filter(i -> i % 3 == 0).boxed().toList());
+        System.out.println("ints = " + IntStream.of(1, 4, 9, 18).filter(i -> i % 3 == 0).boxed().toList());
+
+        List<String> list = List.of("one", "two", "three")
+                .stream()
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("List = " + list);
+
+        //создаём без лишнего List
+        list = Stream.of("one", "two", "three")
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("List from stream = " + list);
+
+        //используют,когда вначале не известен весь набор элементов
+        list = Stream.<String>builder()
+                .add("one")
+                .add("two")
+                .add("three")
+                .build()
+                .map(String::toUpperCase)
+                .toList();
+        System.out.println("List from builder = " + list);
+
+        List<Integer> powers2 = Stream.iterate(1,i -> i*2)
+                .takeWhile(i -> i < 10000) //ограничитель
+                .toList();
+        System.out.println("power2 = " + powers2);
+
+        //передаём без параметра
+        List<Integer> randomInts = Stream.generate(() -> new Random().nextInt())
+                .limit(5) //ограничиваемся в 5 чисел
+                .toList(); //собираем элементы в лист
+        System.out.println("randomInts = " + randomInts);
     }
+
 
     static class CheckEven implements Predicate<Integer> {
 
@@ -71,4 +106,5 @@ public class Main {
             return integer - 9 > 0;
         }
     }
-}
+    }
+
